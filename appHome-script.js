95fe8a63f1c58bf7ebe92d23c5e97a75c763b3a7
Maxx Lehmann <MaxxLehmann@Maxxs-MacBook-Pro.local>
2016-12-02 07:49:02 -0500

    can now create js object and store as local

--- appHome-script.js
+++ appHome-script.js
@@ -13,19 +13,30 @@ if (visited == "true") {
 }
 document.cookie = true
 
-// localStorage.setItem(i, JSON.stringify(["L","1 tbsp","2 lumps"]));
-// JSON.parse(localStorage.getItem(i));
+var values = {}
+$('#scheduler').submit(function() {
+	values.size = $("#sizeIn").val()
+	values.cream = $("#creamIn").val()
+	values.sugar = $("#sugarIn").val()
+	values.Su = $("#Su").prop('checked')
+	values.M = $("#M" ).prop('checked')
+	values.T = $("#T" ).prop('checked')
+	values.W = $("#W" ).prop('checked')
+	values.R = $("#R" ).prop('checked')
+	values.F = $("#F" ).prop('checked')
+	values.St = $("#St").prop('checked')
+	values.timeIn = $("#timeIn").val()
 
-$("#new").click(function() {
+	localStorage.setItem(localStorage.length, JSON.stringify(values))
+    window.location.href = "appHome.html"
+})
+
+
+for (var i = 0; i < localStorage.length; i++) {
+	var data = JSON.parse(localStorage.getItem(i));
+	alert(data.cream)
 	$.get("entry.html", function (data) {
 		$("#schedule").append(data)
 		$(".entry").fadeIn("slow")
 	})
-})
-
-$('#scheduler').submit(function() {
-    var values = $(this).serialize();
-	localStorage.setItem(0, JSON.stringify(values));
-    window.location.href = "appHome.html"
-});
-
+}
