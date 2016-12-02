9adcf90ddf2dd6191f4eaccd75e3be1a4f72cc44
Maxx Lehmann <MaxxLehmann@Maxxs-MacBook-Pro.local>
2016-12-02 01:46:02 -0500

    cleaned up js and home html

--- app-script.js
+++
@@ -1,7 +0,0 @@
-/* MEEM4630 App js*/
-
-var h = document.getElementById("header")
-var t = document.createTextNode("Hello World")
-function appendHello() {
-    h.appendChild(t)
-}
--- appHome-script.js
+++ appHome-script.js
@@ -1,20 +1,21 @@
 /* MEEM4630 App js*/
 
-$("#content").hide()
 var visited = document.cookie
 if (visited == "true") {
-	document.getElementById("content").innerHTML='<object type="text/html" data="homeContent.html" style="height: 455px; width: 310px;"></object>';
-	$("#content").fadeIn("slow")
+	$("#homeContent").fadeIn("slow")
 } else {
-	$("#content").show()
 	$("#helper").fadeIn("1500")
 	$("#helperButton").click(function() {
 		$("#helper").slideUp("slow", function() {
-			$("#content").hide()
-			document.getElementById("content").innerHTML='<object type="text/html" data="homeContent.html" style="height: 455px; width: 310px;"></object>';
-			$("#content").fadeIn("slow")
+			$("#homeContent").fadeIn("slow")
 		})
 	})
 }
-document.cookie = true
+// document.cookie = true
+
+localStorage.setItem("size", "L");
+
+$('#schedule').load('entry.html')
+
+//('<object type="text/html" data="entry.html" style="height: 70px; width: 310px;"></object>')
 
