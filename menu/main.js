9b25a928c39a7f63ddfa659bffde0e24940fbf60
Maxx Lehmann <mglehman@mtu.edu>
2022-05-09 00:28:03 -0500

    make a menu

---
+++ menu/main.js
@@ -0,0 +1,128 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+// run with `tsc -w --strict menu/main.ts`
+var Ingredient = /** @class */ (function () {
+    function Ingredient(name, calories, protein, servingAmount, servingUnit) {
+        if (servingUnit === void 0) { servingUnit = ""; }
+        this.name = name;
+        this.calories = calories;
+        this.protein = protein;
+        this.servingAmount = servingAmount;
+        this.servingUnit = servingUnit;
+    }
+    return Ingredient;
+}());
+var MenuItem = /** @class */ (function (_super) {
+    __extends(MenuItem, _super);
+    function MenuItem(name, recipe, time) {
+        if (time === void 0) { time = '00:00'; }
+        var _this = this;
+        var calories = 0;
+        var protein = 0;
+        for (var _i = 0, recipe_1 = recipe; _i < recipe_1.length; _i++) {
+            var item = recipe_1[_i];
+            calories += item[1].calories * item[0];
+            protein += item[1].protein * item[0];
+        }
+        _this = _super.call(this, name, calories, protein, 1) || this;
+        _this.ingredients = recipe;
+        _this.time = time;
+        return _this;
+    }
+    return MenuItem;
+}(Ingredient));
+// Food -------------------------------------------------------------------------------------------
+var base = {
+    muffin: new Ingredient("Blueberry Muffin", 610, 8, 1),
+    acai: new Ingredient("Acai Bowl", 230, 4, 1),
+    fruit: new Ingredient("Frozen Berries", 80, 1, 0.75, "cup"),
+    icecream: new Ingredient("Ice Cream", 170, 3, 0.67, "cup"),
+    protein: new Ingredient("Protein Powder", 150, 21, 2, "scoop"),
+    egg: new Ingredient("Egg", 70, 6, 1),
+    butter: new Ingredient("Butter", 100, 0, 1, "tbsp"),
+    bread: new Ingredient("Bread", 70, 2, 1, "slice"),
+    ham: new Ingredient("Ham", 70, 9, 2, "oz"),
+    cheese: new Ingredient("Cheese", 110, 7, 0.33, "cup"),
+    ketchup: new Ingredient("Ketchup", 20, 0, 1, "tbsp"),
+    oj: new Ingredient("Orange Juice", 120, 2, 8, "oz")
+};
+var items = {
+    smoothie: new MenuItem("Smoothie", [
+        [1, base.fruit],
+        [1, base.protein],
+        [1, base.icecream]
+    ]),
+    eggSandwich: new MenuItem("Egg Sandwich", [
+        [0.25, base.butter],
+        [2, base.bread],
+        [1, base.egg],
+        [0.5, base.ham],
+        [0.5, base.cheese],
+        [1, base.ketchup]
+    ])
+};
+var food = __assign(__assign({}, items), base);
+var mealPlan = new MenuItem('Meal Plan', [
+    new MenuItem('Pre-Run', [
+        [1, food.smoothie],
+        [1, food.muffin],
+    ], '08:30'),
+    new MenuItem('Breakfast', [
+        [3, food.eggSandwich],
+        [2, food.oj],
+    ], '10:30'),
+].map(function (meal) { return [1, meal]; }));
+// HTML -------------------------------------------------------------------------------------------
+var html = document.getElementById("app");
+if (!html) {
+    throw Error('Element not found');
+}
+var menuItemIngredients = function (recipe) { return recipe.reduce(function (prev, curr) {
+    return prev += "\n    <div class=\"item\">\n      <div>".concat(curr[0] * curr[1].servingAmount, " ").concat(curr[1].servingUnit, "</div>\n      <div>").concat(curr[1].name, "</div>\n      <div>").concat(curr[1].calories * curr[0], "</div>\n      <div>").concat(curr[1].protein * curr[0], "</div>\n    </div>\n  ");
+}, ""); };
+for (var _i = 0, _a = mealPlan.ingredients; _i < _a.length; _i++) {
+    var _b = _a[_i], q = _b[0], meal = _b[1];
+    var contents = "";
+    if (!(meal instanceof MenuItem)) {
+        throw Error('this should be a meal');
+    }
+    for (var _c = 0, _d = meal.ingredients; _c < _d.length; _c++) {
+        var entry = _d[_c];
+        var _e = entry[1], name_1 = _e.name, calories = _e.calories, protein = _e.protein;
+        if (entry[1] instanceof MenuItem) {
+            var details = menuItemIngredients(entry[1].ingredients);
+            contents += "\n        <details>\n          <summary class=\"item\">\n            <div>".concat(entry[0] * entry[1].servingAmount, " ").concat(entry[1].servingUnit, "</div>\n            <div>").concat(name_1, "<span class=\"dots\">&nbsp;&bull;&bull;&bull;</span></div>\n            <div>").concat(calories * entry[0], "</div>\n            <div>").concat(protein * entry[0], "</div>\n          </summary>\n          <div class=\"details\">\n            ").concat(details, "\n          </div>\n        </details>\n      ");
+        }
+        else {
+            contents += "\n        <div class=\"item\">\n          <div>".concat(entry[0] * entry[1].servingAmount).concat(entry[1].servingUnit, "</div>\n          <div>").concat(name_1, "</div>\n          <div>").concat(calories * entry[0], "</div>\n          <div>").concat(protein * entry[0], "</div>\n        </div>\n      ");
+        }
+    }
+    html.insertAdjacentHTML("beforeend", "\n    <div class=\"meal\">\n      <h2>".concat(meal.name, " <small>&nbsp;&nbsp;").concat(meal.time, "</small></h2>\n      ").concat(contents, "\n      <div class=\"item last\">\n        <div><small>Subtotal</small></div>\n        <div></div>\n        <div>").concat(meal.calories, "</div>\n        <div>").concat(meal.protein, "</div>\n      </div>\n    </div>\n  "));
+}
+// Total ------------------------------------------------------------------------------------------
+html.insertAdjacentHTML("beforeend", "\n  <div class=\"meal legend\">\n    <div class=\"item total\">\n      <div>Total</div>\n      <div></div>\n      <div>".concat(mealPlan.calories, "</div>\n      <div>").concat(mealPlan.protein, "</div>\n    </div>\n  </div>\n"));
---
+++ menu/main.ts
@@ -0,0 +1,192 @@
+// run with `tsc -w --strict menu/main.ts`
+class Ingredient {
+  name: string;
+  calories: number;
+  protein: number;
+  servingAmount: number;
+  servingUnit?: string;
+
+  constructor(
+    name: string,
+    calories: number,
+    protein: number,
+    servingAmount: number,
+    servingUnit: string = ""
+  ) {
+    this.name = name;
+    this.calories = calories;
+    this.protein = protein;
+    this.servingAmount = servingAmount;
+    this.servingUnit = servingUnit;
+  }
+}
+
+type RecipeEntryList = [quantity: number, item: Ingredient | MenuItem][];
+
+class MenuItem extends Ingredient {
+  ingredients: RecipeEntryList;
+  time: string;
+
+  constructor(name: string, recipe: RecipeEntryList, time: string = '00:00') {
+    let calories = 0;
+    let protein = 0;
+
+    for (const item of recipe) {
+      calories += item[1].calories * item[0];
+      protein += item[1].protein * item[0];
+    }
+
+    super(name, calories, protein, 1);
+    this.ingredients = recipe;
+    this.time = time;
+  }
+}
+
+
+
+
+
+
+// Food -------------------------------------------------------------------------------------------
+const base = {
+  muffin: new Ingredient("Blueberry Muffin", 610, 8, 1),
+  acai: new Ingredient("Acai Bowl", 230, 4, 1),
+  fruit: new Ingredient("Frozen Berries", 80, 1, 0.75, "cup"),
+  icecream: new Ingredient("Ice Cream", 170, 3, 0.67, "cup"),
+  protein: new Ingredient("Protein Powder", 150, 21, 2, "scoop"),
+  egg: new Ingredient("Egg", 70, 6, 1),
+  butter: new Ingredient("Butter", 100, 0, 1, "tbsp"),
+  bread: new Ingredient("Bread", 70, 2, 1, "slice"),
+  ham: new Ingredient("Ham", 70, 9, 2, "oz"),
+  cheese: new Ingredient("Cheese", 110, 7, 0.33, "cup"),
+  ketchup: new Ingredient("Ketchup", 20, 0, 1, "tbsp"),
+  oj: new Ingredient("Orange Juice", 120, 2, 8, "oz")
+} as const;
+
+const items = {
+  smoothie: new MenuItem("Smoothie", [
+    [1, base.fruit],
+    [1, base.protein],
+    [1, base.icecream]
+  ]),
+  eggSandwich: new MenuItem("Egg Sandwich", [
+    [0.25, base.butter],
+    [2, base.bread],
+    [1, base.egg],
+    [0.5, base.ham],
+    [0.5, base.cheese],
+    [1, base.ketchup]
+  ])
+} as const;
+
+const food = { ...items, ...base };
+
+const mealPlan = new MenuItem('Meal Plan', [
+    new MenuItem('Pre-Run', [
+      [1, food.smoothie],
+      [1, food.muffin],
+    ], '08:30'),
+    new MenuItem('Breakfast', [
+      [3, food.eggSandwich],
+      [2, food.oj],
+    ], '10:30'),
+  ].map(meal => [1, meal])
+);
+
+
+
+
+
+
+
+
+
+
+// HTML -------------------------------------------------------------------------------------------
+let html = document.getElementById("app");
+if (!html) {
+  throw Error('Element not found')
+}
+
+const menuItemIngredients = (recipe: RecipeEntryList) => recipe.reduce<string>((prev, curr) => {
+  return prev += `
+    <div class="item">
+      <div>${curr[0] * curr[1].servingAmount} ${curr[1].servingUnit}</div>
+      <div>${curr[1].name}</div>
+      <div>${curr[1].calories * curr[0]}</div>
+      <div>${curr[1].protein * curr[0]}</div>
+    </div>
+  `;
+}, ``);
+
+
+
+
+
+
+
+for (const [q, meal] of mealPlan.ingredients) {
+  let contents = ``;
+  if (!(meal instanceof MenuItem)) {
+    throw Error('this should be a meal')
+  }
+
+  for (const entry of meal.ingredients) {
+    const { name, calories, protein } = entry[1];
+
+    if (entry[1] instanceof MenuItem) {
+      const details = menuItemIngredients(entry[1].ingredients);
+
+      contents += `
+        <details>
+          <summary class="item">
+            <div>${entry[0] * entry[1].servingAmount} ${entry[1].servingUnit}</div>
+            <div>${name}<span class="dots">&nbsp;&bull;&bull;&bull;</span></div>
+            <div>${calories * entry[0]}</div>
+            <div>${protein * entry[0]}</div>
+          </summary>
+          <div class="details">
+            ${details}
+          </div>
+        </details>
+      `;
+    } else {
+      contents += `
+        <div class="item">
+          <div>${entry[0] * entry[1].servingAmount}${entry[1].servingUnit}</div>
+          <div>${name}</div>
+          <div>${calories * entry[0]}</div>
+          <div>${protein * entry[0]}</div>
+        </div>
+      `;
+    }
+  }
+
+  html.insertAdjacentHTML("beforeend", `
+    <div class="meal">
+      <h2>${meal.name} <small>&nbsp;&nbsp;${meal.time}</small></h2>
+      ${contents}
+      <div class="item last">
+        <div><small>Subtotal</small></div>
+        <div></div>
+        <div>${meal.calories}</div>
+        <div>${meal.protein}</div>
+      </div>
+    </div>
+  `);
+}
+
+
+
+
+// Total ------------------------------------------------------------------------------------------
+html.insertAdjacentHTML("beforeend", `
+  <div class="meal legend">
+    <div class="item total">
+      <div>Total</div>
+      <div></div>
+      <div>${mealPlan.calories}</div>
+      <div>${mealPlan.protein}</div>
+    </div>
+  </div>
+`);
