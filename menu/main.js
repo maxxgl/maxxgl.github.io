0e990a3e806a4614ce670b42fccbdd85511e0ea6
Maxx Lehmann <mglehman@mtu.edu>
2022-05-10 00:03:04 -0500

    update meals

--- menu/main.js
+++ menu/main.js
@@ -76,11 +76,14 @@ var base = {
     mustard: new Ingredient("Mustard", 5, 0, 1, 'tsp'),
     pickle: new Ingredient("Pickle", 0, 0, 1),
     tortilla: new Ingredient("Tortilla", 210, 5, 1),
-    rice: new Ingredient("Rice", 210, 8, 0.75, 'cup'),
+    rice: new Ingredient("Rice", 160, 3, 0.75, 'cup'),
     brocolli: new Ingredient("Brocolli", 30, 3, 1, 'cup'),
     riceKrispie: new Ingredient("Rice Krispie", 90, 0, 1),
     rxBar: new Ingredient("RX Bar", 210, 12, 1),
-    apple: new Ingredient("Apple", 100, 0, 1)
+    apple: new Ingredient("Apple", 100, 0, 1),
+    oatmealPie: new Ingredient("Oatmeal Creme Pie", 330, 3, 1),
+    sanPellegrino: new Ingredient("San Pellegrino", 120, 0, 1),
+    spindrift: new Ingredient("Spindrift", 10, 0, 1)
 };
 var items = {
     smoothie: new MenuItem("Smoothie", [
@@ -105,40 +108,74 @@ var items = {
         [1, base.pickle],
     ]),
     slop: new MenuItem("Slop", [
-        [2, base.rice],
+        [2.4, base.rice],
         [1.5, base.chicken],
         [2, base.brocolli],
     ])
 };
 var food = __assign(__assign({}, items), base);
-var mealPlan = new MenuItem('Meal Plan', [
-    new MenuItem('Pre-Run', [
-        [1, food.smoothie],
-        [0.5, food.muffin],
-    ], '08:30'),
-    new MenuItem('Breakfast', [
-        [1, food.eggSandwich],
-        [1, food.oj],
-    ], '10:30'),
-    new MenuItem('Lunch', [[2, food.wrap]], "13:00"),
-    new MenuItem('Dinner', [[1, food.slop]], '15:00'),
-    new MenuItem('Pre-BJJ', [[1, food.riceKrispie]], '17:30'),
-    new MenuItem('Post-BJJ', [
-        [1, food.rxBar],
-        [1, food.apple],
-    ], '19:30'),
-    new MenuItem('Supper', [[1, food.slop]], '20:00'),
-    new MenuItem('Midnight Snack', [[1, food.slop]], '23:00'),
-].map(function (meal) { return [1, meal]; }));
+var mealPlan = [
+    new MenuItem('Meal Plan: 2022-05-10', [
+        new MenuItem('Pre-Run', [
+            [1, food.rxBar],
+            [0.5, food.muffin],
+        ], '08:30'),
+        new MenuItem('Breakfast', [
+            [1, food.eggSandwich],
+            [1.25, food.oj],
+        ], '10:30'),
+        new MenuItem('Lunch', [
+            [2, food.wrap],
+            [1, food.spindrift],
+        ], "13:00"),
+        new MenuItem('Dinner', [[1, food.slop]], '15:00'),
+        new MenuItem('Pre-BJJ', [[1, food.riceKrispie]], '17:30'),
+        new MenuItem('Post-BJJ', [
+            [1, food.smoothie],
+            [1, food.apple],
+        ], '19:30'),
+        new MenuItem('Supper', [
+            [1, food.slop],
+            [1, food.sanPellegrino],
+        ], '20:00'),
+        new MenuItem('Midnight Snack', [[1, food.slop]], '23:00'),
+    ].map(function (meal) { return [1, meal]; })),
+    new MenuItem('Meal Plan: 2022-05-09', [
+        new MenuItem('Pre-Run', [
+            [1, food.smoothie],
+            [0.5, food.muffin],
+        ], '08:50'),
+        new MenuItem('Breakfast', [
+            [1, food.eggSandwich],
+            [1.25, food.oj],
+        ], '10:50'),
+        new MenuItem('Lunch', [
+            [2, food.wrap],
+            [1, food.spindrift],
+        ], "13:30"),
+        new MenuItem('Dinner', [[1, food.slop]], '16:00'),
+        new MenuItem('Post-BJJ', [
+            [1, food.oatmealPie],
+        ], '19:15'),
+        new MenuItem('Supper', [
+            [1, food.slop],
+            [1, food.sanPellegrino],
+        ], '20:30'),
+        new MenuItem('Midnight Snack', [
+            [1, food.apple],
+            [1, food.rxBar],
+        ], '23:00'),
+    ].map(function (meal) { return [1, meal]; }))
+];
 // HTML -------------------------------------------------------------------------------------------
 var html = document.getElementById("app");
 if (!html) {
     throw Error('Element not found');
 }
 var menuItemIngredients = function (recipe) { return recipe.reduce(function (prev, curr) {
-    return prev += "\n    <div class=\"item\">\n      <div>".concat(curr[0] * curr[1].servingAmount, " ").concat(curr[1].servingUnit, "</div>\n      <div>").concat(curr[1].name, "</div>\n      <div>").concat(Math.round(curr[1].calories * curr[0]), "</div>\n      <div>").concat(Math.round(curr[1].protein * curr[0]), "</div>\n    </div>\n  ");
+    return prev += "\n    <div class=\"item\">\n      <div>".concat(+(curr[0] * curr[1].servingAmount).toFixed(2), " ").concat(curr[1].servingUnit, "</div>\n      <div>").concat(curr[1].name, "</div>\n      <div>").concat(Math.round(curr[1].calories * curr[0]), "</div>\n      <div>").concat(Math.round(curr[1].protein * curr[0]), "</div>\n    </div>\n  ");
 }, ""); };
-for (var _i = 0, _a = mealPlan.ingredients; _i < _a.length; _i++) {
+for (var _i = 0, _a = mealPlan[0].ingredients; _i < _a.length; _i++) {
     var _b = _a[_i], q = _b[0], meal = _b[1];
     var contents = "";
     if (!(meal instanceof MenuItem)) {
@@ -158,4 +195,4 @@ for (var _i = 0, _a = mealPlan.ingredients; _i < _a.length; _i++) {
     html.insertAdjacentHTML("beforeend", "\n    <div class=\"meal\">\n      <div class=\"item header\">\n        <h2>".concat(meal.name, "<small><small>&nbsp;").concat(meal.time, "</small></small></h2>\n        <div>").concat(meal.calories, "</div>\n        <div>").concat(meal.protein, "</div>\n      </div>\n      ").concat(contents, "\n    </div>\n  "));
 }
 // Total ------------------------------------------------------------------------------------------
-html.insertAdjacentHTML("beforeend", "\n  <div class=\"meal legend\">\n    <div class=\"item total\">\n      <div>Total</div>\n      <div></div>\n      <div>".concat(mealPlan.calories, "</div>\n      <div>").concat(mealPlan.protein, "</div>\n    </div>\n  </div>\n"));
+html.insertAdjacentHTML("beforeend", "\n  <div class=\"meal legend\">\n    <div class=\"item total\">\n      <div>Total</div>\n      <div></div>\n      <div>".concat(mealPlan[0].calories, "</div>\n      <div>").concat(mealPlan[0].protein, "</div>\n    </div>\n  </div>\n"));
--- menu/main.ts
+++ menu/main.ts
@@ -49,29 +49,32 @@ class MenuItem extends Ingredient {
 
 // Food -------------------------------------------------------------------------------------------
 const base = {
-  muffin:       new Ingredient("Blueberry Muffin", 610,  8,    1),
-  acai:         new Ingredient("Acai Bowl",        230,  4,    1),
-  fruit:        new Ingredient("Frozen Berries",    80,  1, 0.75,   "cup"),
-  icecream:     new Ingredient("Ice Cream",        170,  3, 0.67,   "cup"),
-  protein:      new Ingredient("Protein Powder",   150, 21,    2, "scoop"),
-  egg:          new Ingredient("Egg",               70,  6,    1),
-  butter:       new Ingredient("Butter",           100,  0,    1,  "tbsp"),
-  bread:        new Ingredient("Bread",             70,  2,    1, "slice"),
-  ham:          new Ingredient("Ham",               70,  9,    2,    "oz"),
-  cheese:       new Ingredient("Cheese",           110,  7, 0.33,   "cup"),
-  ketchup:      new Ingredient("Ketchup",           20,  0,    1,  "tbsp"),
-  oj:           new Ingredient("Orange Juice",     120,  2,    8,    "oz"),
-  chicken:      new Ingredient("Chicken Tender",   100, 23,    4,    'oz'),
-  tomato:       new Ingredient("Cherry Tomatoes",   50,  1,    1,   'cup'),
-  springMix:    new Ingredient("Spring Mix",        20,  2,    2,   'cup'),
-  mustard:      new Ingredient("Mustard",            5,  0,    1,   'tsp'),
-  pickle:       new Ingredient("Pickle",             0,  0,    1),
-  tortilla:     new Ingredient("Tortilla",         210,  5,    1),
-  rice:         new Ingredient("Rice",             210,  8, 0.75,   'cup'),
-  brocolli:     new Ingredient("Brocolli",          30,  3,    1,   'cup'),
-  riceKrispie:  new Ingredient("Rice Krispie",      90,  0,    1),
-  rxBar:        new Ingredient("RX Bar",           210,  12,   1),
-  apple:        new Ingredient("Apple",            100,  0,    1),
+  muffin:        new Ingredient("Blueberry Muffin",  610,  8,    1),
+  acai:          new Ingredient("Acai Bowl",         230,  4,    1),
+  fruit:         new Ingredient("Frozen Berries",     80,  1, 0.75,   "cup"),
+  icecream:      new Ingredient("Ice Cream",         170,  3, 0.67,   "cup"),
+  protein:       new Ingredient("Protein Powder",    150, 21,    2, "scoop"),
+  egg:           new Ingredient("Egg",                70,  6,    1),
+  butter:        new Ingredient("Butter",            100,  0,    1,  "tbsp"),
+  bread:         new Ingredient("Bread",              70,  2,    1, "slice"),
+  ham:           new Ingredient("Ham",                70,  9,    2,    "oz"),
+  cheese:        new Ingredient("Cheese",            110,  7, 0.33,   "cup"),
+  ketchup:       new Ingredient("Ketchup",            20,  0,    1,  "tbsp"),
+  oj:            new Ingredient("Orange Juice",      120,  2,    8,    "oz"),
+  chicken:       new Ingredient("Chicken Tender",    100, 23,    4,    'oz'),
+  tomato:        new Ingredient("Cherry Tomatoes",    50,  1,    1,   'cup'),
+  springMix:     new Ingredient("Spring Mix",         20,  2,    2,   'cup'),
+  mustard:       new Ingredient("Mustard",             5,  0,    1,   'tsp'),
+  pickle:        new Ingredient("Pickle",              0,  0,    1),
+  tortilla:      new Ingredient("Tortilla",          210,  5,    1),
+  rice:          new Ingredient("Rice",              160,  3, 0.75,   'cup'),
+  brocolli:      new Ingredient("Brocolli",           30,  3,    1,   'cup'),
+  riceKrispie:   new Ingredient("Rice Krispie",       90,  0,    1),
+  rxBar:         new Ingredient("RX Bar",            210,  12,   1),
+  apple:         new Ingredient("Apple",             100,  0,    1),
+  oatmealPie:    new Ingredient("Oatmeal Creme Pie", 330,  3,    1),
+  sanPellegrino: new Ingredient("San Pellegrino",    120,  0,    1),
+  spindrift:     new Ingredient("Spindrift",          10,  0,    1),
 } as const;
 
 const items = {
@@ -97,7 +100,7 @@ const items = {
     [1, base.pickle],
   ]),
   slop: new MenuItem("Slop", [
-    [2, base.rice],
+    [2.4, base.rice],
     [1.5, base.chicken],
     [2, base.brocolli],
   ]),
@@ -105,26 +108,60 @@ const items = {
 
 const food = { ...items, ...base };
 
-const mealPlan = new MenuItem('Meal Plan', [
-    new MenuItem('Pre-Run', [
-      [1, food.smoothie],
-      [0.5, food.muffin],
-    ], '08:30'),
-    new MenuItem('Breakfast', [
-      [1, food.eggSandwich],
-      [1, food.oj],
-    ], '10:30'),
-    new MenuItem('Lunch', [[2, food.wrap]], "13:00"),
-    new MenuItem('Dinner', [[1, food.slop]], '15:00'),
-    new MenuItem('Pre-BJJ', [[1, food.riceKrispie]], '17:30'),
-    new MenuItem('Post-BJJ', [
-      [1, food.rxBar],
-      [1, food.apple],
-    ], '19:30'),
-    new MenuItem('Supper', [[1, food.slop]], '20:00'),
-    new MenuItem('Midnight Snack', [[1, food.slop]], '23:00'),
-  ].map(meal => [1, meal])
-);
+const mealPlan = [
+  new MenuItem('Meal Plan: 2022-05-10', [
+      new MenuItem('Pre-Run', [
+        [1, food.rxBar],
+        [0.5, food.muffin],
+      ], '08:30'),
+      new MenuItem('Breakfast', [
+        [1, food.eggSandwich],
+        [1.25, food.oj],
+      ], '10:30'),
+      new MenuItem('Lunch', [
+        [2, food.wrap],
+        [1, food.spindrift],
+      ], "13:00"),
+      new MenuItem('Dinner', [[1, food.slop]], '15:00'),
+      new MenuItem('Pre-BJJ', [[1, food.riceKrispie]], '17:30'),
+      new MenuItem('Post-BJJ', [
+        [1, food.smoothie],
+        [1, food.apple],
+      ], '19:30'),
+      new MenuItem('Supper', [
+        [1, food.slop],
+        [1, food.sanPellegrino],
+      ], '20:00'),
+      new MenuItem('Midnight Snack', [[1, food.slop]], '23:00'),
+    ].map(meal => [1, meal])),
+
+  new MenuItem('Meal Plan: 2022-05-09', [
+      new MenuItem('Pre-Run', [
+        [1, food.smoothie],
+        [0.5, food.muffin],
+      ], '08:50'),
+      new MenuItem('Breakfast', [
+        [1, food.eggSandwich],
+        [1.25, food.oj],
+      ], '10:50'),
+      new MenuItem('Lunch', [
+        [2, food.wrap],
+        [1, food.spindrift],
+      ], "13:30"),
+      new MenuItem('Dinner', [[1, food.slop]], '16:00'),
+      new MenuItem('Post-BJJ', [
+        [1, food.oatmealPie],
+      ], '19:15'),
+      new MenuItem('Supper', [
+        [1, food.slop],
+        [1, food.sanPellegrino],
+      ], '20:30'),
+      new MenuItem('Midnight Snack', [
+        [1, food.apple],
+        [1, food.rxBar],
+      ], '23:00'),
+    ].map(meal => [1, meal]))
+];
 
 
 
@@ -144,7 +181,7 @@ if (!html) {
 const menuItemIngredients = (recipe: RecipeEntryList) => recipe.reduce<string>((prev, curr) => {
   return prev += `
     <div class="item">
-      <div>${curr[0] * curr[1].servingAmount} ${curr[1].servingUnit}</div>
+      <div>${+(curr[0] * curr[1].servingAmount).toFixed(2)} ${curr[1].servingUnit}</div>
       <div>${curr[1].name}</div>
       <div>${Math.round(curr[1].calories * curr[0])}</div>
       <div>${Math.round(curr[1].protein * curr[0])}</div>
@@ -158,7 +195,7 @@ const menuItemIngredients = (recipe: RecipeEntryList) => recipe.reduce<string>((
 
 
 
-for (const [q, meal] of mealPlan.ingredients) {
+for (const [q, meal] of mealPlan[0].ingredients) {
   let contents = ``;
   if (!(meal instanceof MenuItem)) {
     throw Error('this should be a meal')
@@ -216,8 +253,8 @@ html.insertAdjacentHTML("beforeend", `
     <div class="item total">
       <div>Total</div>
       <div></div>
-      <div>${mealPlan.calories}</div>
-      <div>${mealPlan.protein}</div>
+      <div>${mealPlan[0].calories}</div>
+      <div>${mealPlan[0].protein}</div>
     </div>
   </div>
 `);
